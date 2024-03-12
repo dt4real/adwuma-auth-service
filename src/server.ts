1ef1140553d850df38fdcf3fa4f4580961a355d6
dt4real <dotchere95@gmail.com>
2024-03-12 23:29:05 +0100

    Introduce endpoint to seed test users

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ import helmet from 'helmet';
 import cors from 'cors';
 import { verify } from 'jsonwebtoken';
 import compression from 'compression';
-import { checkConnection } from '@auth/elasticsearch';
+import { checkConnection, createIndex } from '@auth/elasticsearch';
 import { Channel } from 'amqplib';
 import { createConnection } from '@auth/queues/connection';
 import { appRoutes } from '@auth/routes';
@@ -67,6 +67,7 @@ async function startQueues(): Promise<void> {
 
 function startElasticSearch(): void {
   checkConnection();
+  createIndex('gigs');
 }
 
 function authErrorHandler(app: Application): void {
