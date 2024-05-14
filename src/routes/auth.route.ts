f73bfc5dd94cedb6e20dbc52699c3dd77e1a40a9
dt4real <dotchere95@gmail.com>
2024-05-14 12:53:44 +0200

    Implement OTP functionalities

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -2,6 +2,7 @@ import { changePassword, forgotPassword, resetPassword } from '@auth/controllers
 import { read } from '@auth/controllers/signin.controller';
 import { create } from '@auth/controllers/signup.controller';
 import { update } from '@auth/controllers/verify-email.controller';
+import { updateOTP } from '@auth/controllers/verify-opt.controller';
 import express, { Router } from 'express';
 
 const router: Router = express.Router();
@@ -10,6 +11,7 @@ export function authRoutes(): Router {
   router.post('/signup', create);
   router.post('/signin', read);
   router.put('/verify-email', update);
+  router.put('/verify-otp/:otp', updateOTP);
   router.put('/forgot-password', forgotPassword);
   router.put('/reset-password/:token', resetPassword);
   router.put('/change-password', changePassword);
